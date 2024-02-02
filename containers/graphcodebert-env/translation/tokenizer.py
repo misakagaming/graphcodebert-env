822b621e4a5f6a37b2e4bb35dab46f982c51f3d4
misakagaming <eray.erer@hotmail.com>
2024-02-02 14:42:03 +0300

    updated transformer for new file path

--- containers/graphcodebert-env/translation/tokenizer.py
+++ containers/graphcodebert-env/translation/tokenizer.py
@@ -2,7 +2,7 @@ from tokenizers import ByteLevelBPETokenizer
 from tokenizers.processors import BertProcessing
 from pathlib import Path
 
-paths = [str(x) for x in Path(r"C:/Users/eraye/OneDrive/Masaüstü/environment/containers/graphcodebert-env/gen2java").glob("*.txt")]
+paths = [str(x) for x in Path("C:\Users\eraye\Desktop\graphcodebert-env\containers\graphcodebert-env\gen2java\cs_translation").glob("*.txt")]
 
 # Initialize a tokenizer
 tokenizer = ByteLevelBPETokenizer(lowercase=True)
@@ -18,4 +18,4 @@ tokenizer.train(files=paths, vocab_size=50265, min_frequency=2,
                                 "<mask>",
 ])
 #Save the Tokenizer to disk
-tokenizer.save_model(r"C:\Users\eraye\OneDrive\Masaüstü\environment\containers\graphcodebert-env\translation\tokenizer")
+tokenizer.save_model("C:\Users\eraye\Desktop\graphcodebert-env\containers\graphcodebert-env\translation\tokenizer")
