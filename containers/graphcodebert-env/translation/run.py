0f5376d6e4f06139ac8b7482b87e46013fc5dc62
misakagaming <eray.erer@hotmail.com>
2024-02-04 17:31:02 +0300

    new segmented cases and trying None

--- containers/graphcodebert-env/translation/run.py
+++ containers/graphcodebert-env/translation/run.py
@@ -400,7 +400,7 @@ def main():
     config = config_class.from_pretrained(args.config_name)
     tokenizer = tokenizer_class.from_pretrained(r"tokenizer")
     #budild model
-    encoder = model_class.from_pretrained(args.model_name_or_path,config=config, ignore_mismatched_sizes=True)
+    encoder = model_class.from_pretrained(None,config=config, ignore_mismatched_sizes=True)
     
     decoder_layer = nn.TransformerDecoderLayer(d_model=config.hidden_size, nhead=config.num_attention_heads)
     decoder = nn.TransformerDecoder(decoder_layer, num_layers=6)
